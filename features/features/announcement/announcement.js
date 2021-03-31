cbf45f053478c0e358fe2ab9f8899b9604220ceb
StevenNguyen <fall.legendary1992@gmail.com>
2021-03-31 23:39:36 +0700

    update

--- features/features/announcement/announcement.js
+++ features/features/announcement/announcement.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 
+// module.exports = async (client) => {}
 
 module.exports = async (client) => {
 
@@ -8,10 +9,10 @@ module.exports = async (client) => {
         return
     }
 
-    // client.channels.cache.get(`channelID`)
-    const embed = new Discord.MessageEmbed()
+    // general
+    const embed1 = new Discord.MessageEmbed()
         .setTitle('Leidenschaftlich')
-        .setDescription('Chào mừng các bạn đến với thành phố Leidenschaftlich. Đây là một thế giới nơi công nghệ và phép màu dung hòa. Để tham gia thì bạn phải là regular của server và lấy role "adventure" ở channel phía dưới (sẽ mở khi bạn đạt cấp độ regular).\nHiện game đang trong quá trình phát triển và sẽ hoàn thiện hơn trong những ngày tiếp theo.\n\nPrefix của Violet bot sẽ là "!p"')
+        .setDescription('Chào mừng các bạn đến với thành phố Leidenschaftlich. Đây là một thế giới nơi công nghệ và phép màu dung hòa. Để tham gia thì bạn phải là regular của server và lấy role "adventure" ở channel phía dưới (sẽ mở khi bạn đạt cấp độ regular).\nHiện game đang trong quá trình phát triển và sẽ hoàn thiện hơn trong những ngày tiếp theo.\n\nPrefix của Violet bot sẽ là "!!"')
         .setThumbnail('https://i.imgur.com/sEcD1lZ.jpg')
         .setImage('https://i.imgur.com/bs3ESNo.jpg')
         // .addFields(
@@ -19,17 +20,54 @@ module.exports = async (client) => {
         // )
         .setFooter(`v4v`, 'https://i.imgur.com/pmDv6Hb.png')
         .setTimestamp()
-        .setColor(`#00AAF`)
+        .setColor(`#FFFDC0`)
 
-    channel.messages.fetch().then((messages) => {
-        if (messages.size === 0) {
-            // Send a new message
-            channel.send(embed)
-        } else {
-            for (const message of messages) {
-                message[1].edit(embed)
-            }
-        }
-    })
+    // Dungeon
+    const embed2 = new Discord.MessageEmbed()
+        .setTitle('Elfazz')
+        .setDescription('Elfazz - Ngôi mộ của những vị vua cổ xưa. Tọa lạc tại Tây Bắc thành phố Leidenschaftlich. Khi đến gần, ai cũng cảm thấy bầu không khí còn vương vấn đôi chút ma phép nhiệm màu. Những họa tiết điêu khắc tỉ mỉ trên từng cánh cửa phác họa nên thời huy hoàng một trong những nền văn minh sớm nhất nhân loại. Tiếc là giờ đây tất cả chi là đống tro tàn. Nơi đây chứa đầy cạm bẫy, các adventure phải hết sức cẩn thận.')
+        .setThumbnail('https://i.imgur.com/0piFMAq.png')
+        // .setImage('https://i.imgur.com/7UZ7vwz.jpg')
+        .addFields(
+            { name: 'Type:', value: `dungeon, fantasy` },
+        )
+        .setFooter(`v4v`, 'https://i.imgur.com/pmDv6Hb.png')
+        .setTimestamp()
+        .setColor(`#FFFDC0`)
+
+    // const embed3 = new Discord.MessageEmbed()
+    //     .setTitle('Các lệnh cơ bản khác')
+    //     .setDescription('Bot sẽ có prefix là "!!"')
+    //     .setThumbnail('https://i.imgur.com/JDWQ8Mp.jpg')
+    //     .setImage('')
+    //     .addFields(
+    //         { name: 'Lệnh hiện có:', value: `"daily" : Nhận :cross: exp và :yen: tiền mỗi ngày.\n"info" : xem profile của adventure.\n` }
+    //     )
+    //     .setFooter(`v4v`, 'https://i.imgur.com/pmDv6Hb.png')
+    //     .setTimestamp()
+    //     .setColor(`#FFFDC0`)
+
+    // const embedArr = [embed1, embed2]
+
+    // channel.send(embed2)
+
+    // channel.messages.fetch('826797090499854406')
+    //     .then(message => message.edit(''))
+    //     .catch(console.error);
+
+    // channel.messages.fetch().then(async (messages) => {
+
+    //     const msgIdArr = []
+    //     for (const message of messages) {
+    //         msgIdArr.push(message[0])
+    //     }
+
+    //     msgIdArr.reverse().forEach(async (item, index) => {
+    //         channel.messages.fetch(item)
+    //             .then(message => message.edit(embedArr[index]))
+    //             .catch(console.error);
+    //     })
+
+    // })
 
 }
--- features/features/announcement/instruction.js
+++ features/features/announcement/instruction.js
@@ -26,11 +26,12 @@ module.exports = async (client) => {
     // Dungeon
     const embed2 = new Discord.MessageEmbed()
         .setTitle('Dungeon')
-        .setDescription('Leidenschaftlich là một thế giới pha trộn giữa công nghệ và phép thuật, nhưng không ai biết Leidenschaftlich ra đời từ khi nào. Xung quanh thành phố đầy ánh hào quang là một thế giới xa xưa nay chỉ còn là đống tro tàn.\nChức năng thám hiểm dungeon dành cho adventure. Yêu cầu 1 token để bắt đầu, bạn sẽ phải đưa ra quyết định ảnh hưởng đến kết quả và phần thưởng.')
+        .setDescription('Leidenschaftlich là một thế giới pha trộn giữa công nghệ và phép thuật, nhưng không ai biết Leidenschaftlich ra đời từ khi nào. Xung quanh thành phố đầy ánh hào quang là một thế giới xa xưa nay chỉ còn là đống tro tàn.\n\nChức năng thám hiểm dungeon dành cho adventure. Yêu cầu 1 token để bắt đầu, bạn sẽ phải đưa ra quyết định ảnh hưởng đến kết quả và phần thưởng.')
         .setThumbnail('https://i.imgur.com/7UZ7vwz.jpg')
         // .setImage('https://i.imgur.com/7UZ7vwz.jpg')
         .addFields(
             { name: 'Lệnh hiện có:', value: `dun` },
+            { name: 'Cơ chế', value: `Tình huống ngẫu nhiên được đưa ra, bạn chọn câu trả lời. Mỗi một câu trả lời sẽ có hai tình huống "thắng" và "thua", level cao sẽ tăng khả năng thắng của bạn. Một tình huống sẽ mang lại nhiều chiến lợi phẩm.` },
         )
         .setFooter(`v4v`, 'https://i.imgur.com/pmDv6Hb.png')
         .setTimestamp()
@@ -52,23 +53,23 @@ module.exports = async (client) => {
 
     // channel.send(embed3)
 
-    // channel.messages.fetch('826797090499854406')
-    //     .then(message => message.edit(''))
-    //     .catch(console.error);
+    channel.messages.fetch('826797090499854406')
+        .then(message => message.edit(''))
+        .catch(console.error);
 
-    // channel.messages.fetch().then(async (messages) => {
+    channel.messages.fetch().then(async (messages) => {
 
-    //     const msgIdArr = []
-    //     for (const message of messages) {
-    //         msgIdArr.push(message[0])
-    //     }
+        const msgIdArr = []
+        for (const message of messages) {
+            msgIdArr.push(message[0])
+        }
 
-    //     msgIdArr.reverse().forEach(async (item, index) => {
-    //         channel.messages.fetch(item)
-    //             .then(message => message.edit(embedArr[index]))
-    //             .catch(console.error);
-    //     })
+        msgIdArr.reverse().forEach(async (item, index) => {
+            channel.messages.fetch(item)
+                .then(message => message.edit(embedArr[index]))
+                .catch(console.error);
+        })
 
-    // })
+    })
 
 }
