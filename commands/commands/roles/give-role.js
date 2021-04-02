ca517e4ab50c75f68278e47659c1a05f1913a8a4
StevenNguyen <fall.legendary1992@gmail.com>
2021-04-02 17:07:46 +0700

    update

--- commands/commands/roles/give-role.js
+++ commands/commands/roles/give-role.js
@@ -1,25 +1,28 @@
 module.exports = {
-    commands: 'giverole',
-    minArgs: 2,
+    commands: 'give',
+    minArgs: 0,
     expectedArgs: "<Target user's @>",
     permission: 'ADMINISTRATOR',
     callback: (message, arguments) => {
-        const targetUser = message.mentions.users.first()
-        if (!targetUser) return message.reply('Please specify someone to give a role to')
 
-        arguments.shift()
+        // const targetUser = message.mentions.users.first()
+        // if (!targetUser) return message.reply('Please specify someone to give a role to')
 
-        const roleName = arguments.join(' ')
+        // arguments.shift()
+
+        // const roleName = arguments.join(' ')
         const { guild } = message
+        const guildId = message.guild.id
+        const userId = message.author.id
 
-        const role = guild.roles.cache.find((role) => {
-            return role.name === roleName
-        })
+        // const role = guild.roles.cache.find((role) => {
+        //     return role.name === roleName
+        // })
 
-        if (!role) return message.reply(`There's no role with the name ${roleName}`)
+        // if (!role) return message.reply(`There's no role with the name ${roleName}`)
 
-        const member = guild.members.cache.get(targetUser.id)
-        member.roles.add(role)
-        message.reply(`That user now has the ${roleName}`)
+        const member = guild.members.cache.get(userId)
+        member.roles.add('wound')
+
     }
 }
