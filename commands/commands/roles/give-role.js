c7f8fcbef11d21aa853506f4b148d6883487bf43
Phạm Trung Hiếu <34348907+Rohupt@users.noreply.github.com>
2021-04-06 22:36:13 +0700

    Update give-role.js

--- commands/commands/roles/give-role.js
+++ commands/commands/roles/give-role.js
@@ -2,7 +2,7 @@ module.exports = {
     commands: 'giverole',
     minArgs: 2,
     expectedArgs: "<Target user's @> <The role name>",
-    permissions: 'ADMINISTRATOR',
+    requiredChannel: 'bot-test',
     callback: (message, arguments) => {
         const targetUser = message.mentions.users.first()
         if (!targetUser) {
