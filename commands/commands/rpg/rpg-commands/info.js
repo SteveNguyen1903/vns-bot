79ec1e3e99d1cd17d6bf9a73bff177b17590f146
StevenNguyen <fall.legendary1992@gmail.com>
2021-04-20 16:41:33 +0700

    update info

--- commands/commands/rpg/rpg-commands/info.js
+++ commands/commands/rpg/rpg-commands/info.js
@@ -1,6 +1,7 @@
 const economy = require('@features/economy')
 const Discord = require('discord.js')
 const partnerSchema = require('@schema/partner-schema')
+const pn = require('@util/partner/partner-features')
 
 const getNeededXP = (level) => level * level * 100
 const maxHP = (level) => level * 20 + 100
@@ -17,25 +18,34 @@ module.exports = {
 		const userId = message.author.id
 		const inventory = await economy.showProfile(guildId, userId)
 		const userCharsDb = await partnerSchema.findOne({ userId, guildId })
+		const listCharacters = await pn.getListCharacters()
+
 		if (!inventory) return message.reply('Hãy đánh lệnh daily để tạo profile!')
+
+		let partner = userCharsDb?.currentPartner ? userCharsDb.currentPartner : 'chưa có'
+		let marry = userCharsDb?.currentMarry ? userCharsDb.currentMarry : 'chưa kết hôn'
+
+		let partnerAva = listCharacters.filter((character) => character.name.includes(partner))
+		partnerAva = partnerAva.length > 0 ? partnerAva[0].img[Math.floor(Math.random() * partnerAva[0].img.length)] : ''
+
+		let marryAva = listCharacters.filter((character) => character.name.includes(marry))
+		marryAva = marryAva.length > 0 ? marryAva[0].img[Math.floor(Math.random() * marryAva[0].img.length)] : ''
 
 		const embed = new Discord.MessageEmbed()
 			.setColor(`#FFFDC0`)
-			.setTitle(`Thế giới Leidenschaftlich`)
+			// .setTitle(`Thế giới Leidenschaftlich`)
 			// .setURL('https://discord.js.org/')
-			// .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-			.setDescription(`Hồ sơ hành gia <@${userId}>`)
+			.setAuthor(`Partner: ${partner}`, `${partnerAva}`)
+			.setDescription(`Thế giới Leidenschaftlich\nHồ sơ hành gia <@${userId}>`)
 			.setThumbnail(`${message.author.displayAvatarURL()}`)
 			.addFields(
 				{ name: 'Thông tin', value: `:yen: Tiền: ${inventory.coins}\n:cross: EXP: ${inventory.xp}/${getNeededXP(inventory.level)} đến **level ${inventory.level + 1}**\n:drop_of_blood: HP: ${inventory.hp}/${maxHP(inventory.level)}` },
-				{ name: 'Partner', value: `:handshake: Partner hiện tại: ${userCharsDb?.currentPartner ? userCharsDb.currentPartner : 'chưa có'}\n:ring: Đã kết hôn: ${userCharsDb?.currentMarry ? userCharsDb.currentMarry : 'chưa kết hôn'}` },
 				{
 					name: 'Hòm đồ',
 					value: `:coin: Token: ${inventory.items.token}\n:envelope: Letter: ${inventory.items.letter}\n:test_tube: Potion: ${inventory.items.potion}\n:gift: Gift: ${inventory.items.gift}`,
 				},
 			)
-			.setTimestamp()
-			.setFooter('Developed by v4v', 'https://i.imgur.com/pmDv6Hb.png')
+			.setFooter(`Kết hôn: ${marry}`, `${marryAva}`)
 		message.channel.send(embed)
 	},
 }
